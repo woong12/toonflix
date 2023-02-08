c824cd3cd5dc378bb38d2a25f50be5065d686dc9
woong <koohw0109@gmail.com>
2023-02-08 23:47:29 +0900

    transfer

--- lib/main.dart
+++ lib/main.dart
@@ -48,11 +48,13 @@ class App extends StatelessWidget {
               const SizedBox(
                 height: 80,
               ),
-              Text(
-                'Total Balance',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white.withOpacity(0.8),
+              Container(
+                child: Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white.withOpacity(0.8),
+                  ),
                 ),
               ),
               const SizedBox(
