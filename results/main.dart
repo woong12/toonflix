a83672b1803106a121ad3dea70b4740250b24e38
woong <koohw0109@gmail.com>
2023-03-13 23:25:28 +0900

    required

---
+++ results/main.dart
@@ -0,0 +1,32 @@
+/* import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State createState() => _AppState();
+}
+
+class _AppState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+ */
