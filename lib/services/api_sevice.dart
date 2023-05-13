3e25d81e2b89c95eacf9971d6c8cc6f80f47e5ba
woong <koohw0109@gmail.com>
2023-05-13 19:05:34 +0900

    data fetching

--- lib/services/api_sevice.dart
+++ lib/services/api_sevice.dart
@@ -1,16 +1,16 @@
-// import 'pakage:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
-// class ApiService {
-//   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-//   final String today = "today";
+class ApiService {
+  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
 
-//   void getTodaysToons() async {
-//     final url = Uri.parse('$baseUrl/$today');
-//     final response = await http.get(url);
-//     if (response.statusCode == 200) {
-//       print(response.body);
-//       return;
-//     }
-//     throw Error();
-//   }
-// }
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return;
+    }
+    throw Error();
+  }
+}
